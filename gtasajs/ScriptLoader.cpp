8134093aa4bbb4e73dd338a57cd37ced2cc316a2
Jordy Hulck <jordy.hulck@outlook.com>
2016-05-06 23:53:11 +0200

    Load scripts from js folder
    Else create js folder

--- gtasajs/ScriptLoader.cpp
+++ gtasajs/ScriptLoader.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "ScriptLoader.h"
+#include <boost\filesystem.hpp>
+#include <sstream>
 
+namespace fs = boost::filesystem;
+using namespace fs;
 
 ScriptLoader::ScriptLoader(JsContextRef* context)
 {
@@ -15,6 +19,27 @@ ScriptLoader::~ScriptLoader()
 
 void ScriptLoader::LoadScripts()
 {
+	path js(L"js");
+	if (is_directory(js))
+	{
+		for (directory_entry& entry : directory_iterator(js))
+		{
+			path file = entry.path();
+			if (is_regular_file(file) && file.has_extension() && file.extension() == ".js")
+			{
+				fs::ifstream fileStream(file);
+				ostringstream stringStream;
+				stringStream << fileStream.rdbuf();
+				fileStream.close();
+				string script = stringStream.str();
+				LoadScript(script);
+			}
+		}
+	}
+	else
+	{
+		create_directory(js);
+	}
 }
 
 
